17be2f044d35b6e8b0dd53537a30d4d6e6a30496
Artur Signell <artur@vaadin.com>
2017-02-02 13:48:13 +0200

    Run Sonar for PRs

---
+++ .travis.script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# TRAVIS_PULL_REQUEST == "false" for a normal branch commit, the PR number for a PR
+# TRAVIS_BRANCH == target of normal commit or target of PR
+# TRAVIS_SECURE_ENV_VARS == true if encrypted variables, e.g. SONAR_HOST is available
+# TRAVIS_REPO_SLUG == the repository, e.g. vaadin/vaadin
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
+then
+	# Pull request for master with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
+	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+else
+	# Something else than a "safe" pull request
+	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE verify
+fi
