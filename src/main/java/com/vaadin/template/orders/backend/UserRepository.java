aafca07b85683f6b5f8c90297c179f2c5891e14e
Artur <artur@vaadin.com>
2017-02-06 16:05:44 +0200

    Basic authentication and access control using a HTML login form (#10)

--- src/main/java/com/vaadin/template/orders/backend/UserRepository.java
+++ src/main/java/com/vaadin/template/orders/backend/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vaadin.template.orders.backend.data.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByEmail(String email);
 }
