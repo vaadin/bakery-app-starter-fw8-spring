04ced627f05f5ba9593c7a7cc68e7c23226c23d6
Artur <artur@vaadin.com>
2017-02-10 10:23:15 +0200

    Include board license (#15)

--- .travis.script.sh
+++ .travis.script.sh
@@ -7,14 +7,17 @@
 
 # Exclude the compiled theme from Sonar analysis
 SONAR_EXCLUSIONS=src/main/webapp/VAADIN/themes/orderstheme/styles.css,src/main/webapp/VAADIN/themes/orderstheme/designs.scss,src/main/java/com/vaadin/template/orders/ui/**/*Design.java
+TESTBENCH=-Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE
+CHARTS=-Dvaadin.charts.developer.license=$CHARTS_LICENSE
+BOARD=-Dvaadin.board.developer.license=$BOARD_LICENSE
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]
 then
 	# Pull request for master with secure vars (SONAR_GITHUB_OAUTH, SONAR_HOST) available
-	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dvaadin.charts.developer.license=$CHARTS_LICENSE -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.exclusions=$SONAR_EXCLUSIONS org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
+	mvn -B -e -V $TESTBENCH $CHARTS $BOARD -Dsonar.verbose=true -Dsonar.analysis.mode=issues -Dsonar.github.repository=$TRAVIS_REPO_SLUG -Dsonar.host.url=$SONAR_HOST -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH -Dsonar.login=$SONAR_LOGIN -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.exclusions=$SONAR_EXCLUSIONS org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
 then
 	# master build
 	# Charts license is only needed because CDN is triggered also in the "verify" step for some reason
-	mvn -B -e -V -Dmaven.javadoc.skip=false -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -Dvaadin.charts.developer.license=$CHARTS_LICENSE -Dsonar.verbose=true -Dsonar.analysis.mode=publish -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN -Dsonar.exclusions=$SONAR_EXCLUSIONS verify sonar:sonar
+	mvn -B -e -V -Dmaven.javadoc.skip=false $TESTBENCH $CHARTS $BOARD -Dsonar.verbose=true -Dsonar.analysis.mode=publish -Dsonar.host.url=$SONAR_HOST -Dsonar.login=$SONAR_LOGIN -Dsonar.exclusions=$SONAR_EXCLUSIONS verify sonar:sonar
 fi
