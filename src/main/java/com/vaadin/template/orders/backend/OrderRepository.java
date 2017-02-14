24a7a9562736d03b97c6eea8dde19a83ff319b97
Artur <artur@vaadin.com>
2017-02-14 15:42:02 +0200

    Only show upcoming orders (#30)

--- src/main/java/com/vaadin/template/orders/backend/OrderRepository.java
+++ src/main/java/com/vaadin/template/orders/backend/OrderRepository.java
@@ -1,8 +1,19 @@
 package com.vaadin.template.orders.backend;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.vaadin.template.orders.backend.data.OrderState;
 import com.vaadin.template.orders.backend.data.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findByDueAfterAndStateInOrderByDueAsc(LocalDateTime date,
+            Collection<OrderState> states, Pageable page);
+
+    long countByDueAfterAndStateIn(LocalDateTime date,
+            Collection<OrderState> states);
 }
