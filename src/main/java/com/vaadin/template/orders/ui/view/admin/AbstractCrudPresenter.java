8f1de1e3b444ed0c7141f3f7dd520c7a726fee09
Artur Signell <artur@vaadin.com>
2017-03-01 22:41:26 +0200

    Generalization of admin view

---
+++ src/main/java/com/vaadin/template/orders/ui/view/admin/AbstractCrudPresenter.java
@@ -0,0 +1,84 @@
+package com.vaadin.template.orders.ui.view.admin;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+
+public abstract class AbstractCrudPresenter<T, V extends AbstractCrudView<T>> {
+
+    private V view;
+
+    public abstract void filterGrid(String filter);
+
+    protected abstract T getCopy(T entity);
+
+    protected abstract PageableDataProvider<T, Object> getGridDataProvider();
+
+    protected abstract T createEntity();
+
+    protected abstract T saveEntity(T editItem);
+
+    protected abstract void deleteEntity(T entity);
+
+    public void init(V view) {
+        this.view = view;
+    }
+
+    protected V getView() {
+        return view;
+    }
+
+    public void editRequest(T entity) {
+        // Fetch a fresh item so we have the latest changes (less optimistic
+        // locking problems) and so that we are not editing the data shown in
+        // the grid (change data -> cancel should not update the grid)
+        T freshCopy = getCopy(entity);
+        getView().editItem(freshCopy, false);
+    }
+
+    public void addNewClicked() {
+        T entity = createEntity();
+        getView().editItem(entity, true);
+    }
+
+    public void updateClicked(boolean isNew) {
+        T saved;
+        try {
+            saved = saveEntity(view.getEditItem());
+        } catch (Exception e) {
+            // This could be either that somebody else edited the item -> should
+            // tell to refresh
+            // or then invalid data -> should not happen if validators are in
+            // place
+            Notification.show(
+                    "A problem occured while saving the data. Please check the fields.",
+                    Type.ERROR_MESSAGE);
+            return;
+        }
+        if (isNew) {
+            getGridDataProvider().refreshAll();
+        } else {
+            getGridDataProvider().refreshItem(saved);
+        }
+        view.stopEditing();
+    }
+
+    public void deleteClicked() {
+        try {
+            deleteEntity(getView().getEditItem());
+        } catch (DataIntegrityViolationException e) {
+            Notification.show(
+                    "The given entity cannot be deleted as there are references to it in the database",
+                    Type.ERROR_MESSAGE);
+            return;
+        }
+        getGridDataProvider().refreshAll();
+        getView().stopEditing();
+    }
+
+    public void cancelClicked() {
+        getView().stopEditing();
+    }
+
+}
---
+++ src/main/java/com/vaadin/template/orders/ui/view/admin/AbstractCrudView.java
@@ -0,0 +1,110 @@
+package com.vaadin.template.orders.ui.view.admin;
+
+import org.springframework.security.access.annotation.Secured;
+
+import com.vaadin.data.Binder;
+import com.vaadin.template.orders.backend.data.Role;
+import com.vaadin.template.orders.ui.HasComponent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.components.grid.SingleSelectionModel;
+
+@Secured(Role.ADMIN)
+public abstract class AbstractCrudView<T> implements HasComponent {
+
+    private final Class<T> entityType;
+    private final Binder<T> binder;
+
+    protected AbstractCrudView(Class<T> entityType) {
+        this.entityType = entityType;
+        this.binder = new Binder<>(entityType);
+    }
+
+    public void editItem(T entity, boolean isNew) {
+        getBinder().setBean(entity);
+        getForm().setEnabled(true);
+        getAdd().setEnabled(false);
+        getDelete().setEnabled(!isNew);
+        getUpdate().setCaption(isNew ? "Add" : "Update");
+    }
+
+    public T getEditItem() {
+        return getBinder().getBean();
+    }
+
+    protected Binder<T> getBinder() {
+        return binder;
+    }
+
+    public void stopEditing() {
+        getForm().setEnabled(false);
+        getAdd().setEnabled(true);
+        getBinder().setBean(null);
+        getGrid().deselectAll();
+    }
+
+    protected void init() {
+        // Because of https://github.com/vaadin/framework/issues/8467
+        Grid<T> oldGrid = getGrid();
+        Grid<T> list = new Grid<>(entityType);
+        list.setSizeFull();
+        ((ComponentContainer) oldGrid.getParent()).replaceComponent(oldGrid,
+                list);
+        setGrid(list);
+        // End of workaround
+
+        getGrid().addSelectionListener(e -> {
+            if (!e.isUserOriginated()) {
+                return;
+            }
+
+            if (e.getFirstSelectedItem().isPresent()) {
+                getPresenter().editRequest(e.getFirstSelectedItem().get());
+            } else {
+                throw new IllegalStateException(
+                        "This should never happen as deselection is not allowed");
+            }
+        });
+
+        // Force user to choose save or cancel in form once enabled
+        ((SingleSelectionModel<T>) getGrid().getSelectionModel())
+                .setDeselectAllowed(false);
+        getGrid().setDataProvider(getPresenter().getGridDataProvider());
+
+        // Button logic
+        getUpdate().addClickListener(event -> {
+            boolean isNew = list.getSelectedItems().isEmpty();
+            getPresenter().updateClicked(isNew);
+        });
+        getDelete().addClickListener(event -> getPresenter().deleteClicked());
+        getCancel().addClickListener(event -> getPresenter().cancelClicked());
+        getAdd().addClickListener(event -> getPresenter().addNewClicked());
+
+        // Search functionality
+        getSearch().addValueChangeListener(
+                event -> getPresenter().filterGrid(event.getValue()));
+
+    }
+
+    protected abstract AbstractCrudPresenter<T, ? extends AbstractCrudView<T>> getPresenter();
+
+    protected abstract Grid<T> getGrid();
+
+    protected abstract void setGrid(Grid<T> grid);
+
+    protected abstract Component getForm();
+
+    protected abstract Button getAdd();
+
+    protected abstract Button getCancel();
+
+    protected abstract Button getDelete();
+
+    protected abstract Button getUpdate();
+
+    protected abstract TextField getSearch();
+
+}
