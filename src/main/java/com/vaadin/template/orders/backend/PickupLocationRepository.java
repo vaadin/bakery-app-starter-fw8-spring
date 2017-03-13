24477be37cd97c7d1e6158564369adae3eacd838
Artur Signell <artur@vaadin.com>
2017-03-13 11:00:32 +0200

    Extract pickup location combobox and price converter

--- src/main/java/com/vaadin/template/orders/backend/PickupLocationRepository.java
+++ src/main/java/com/vaadin/template/orders/backend/PickupLocationRepository.java
@@ -1,9 +1,16 @@
 package com.vaadin.template.orders.backend;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vaadin.template.orders.backend.data.entity.PickupLocation;
 
 public interface PickupLocationRepository
         extends JpaRepository<PickupLocation, Long> {
+
+    Page<PickupLocation> findByNameLikeIgnoreCaseOrderByName(String nameFilter,
+            Pageable pageable);
+
+    int countByNameLikeIgnoreCase(String nameFilter);
 }
---
+++ src/main/java/com/vaadin/template/orders/ui/components/PickupLocationComboBox.java
@@ -0,0 +1,32 @@
+package com.vaadin.template.orders.ui.components;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.template.orders.backend.data.entity.PickupLocation;
+import com.vaadin.template.orders.ui.PrototypeScope;
+import com.vaadin.ui.ComboBox;
+
+@SpringComponent
+@PrototypeScope
+public class PickupLocationComboBox extends ComboBox<PickupLocation> {
+
+    // Singleton data provider which knows which products are available and is
+    // informed when they are updated, deleted or new ones are added
+    @Autowired
+    private PickupLocationComboBoxDataProvider dataProvider;
+
+    public PickupLocationComboBox() {
+        setEmptySelectionAllowed(false);
+        setPlaceholder("Pickup location");
+        setItemCaptionGenerator(PickupLocation::getName);
+    }
+
+    @PostConstruct
+    public void init() {
+        setDataProvider(dataProvider);
+    }
+
+}
