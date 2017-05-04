dfdd7a0a105384af1dd3fdb09508f8590c972cdb
Artur <artur@vaadin.com>
2017-05-04 21:28:11 +0300

    Really ignore frontend files in Sonar (#122)

--- .travis.script.sh
+++ .travis.script.sh
@@ -8,7 +8,7 @@
 # Exclude the compiled theme from Sonar analysis
 SONAR_EXCLUSIONS=src/main/webapp/VAADIN/themes/orderstheme/styles.css
 SONAR_EXCLUSIONS+=,src/main/webapp/VAADIN/themes/orderstheme/designs.scss
-SONAR_EXCLUSIONS+=,src/main/webapp/VAADIN/frontend
+SONAR_EXCLUSIONS+=,src/main/webapp/VAADIN/frontend/**
 SONAR_EXCLUSIONS+=,src/main/java/com/vaadin/template/orders/ui/**/*Design.java
 
 TESTBENCH=-Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE
