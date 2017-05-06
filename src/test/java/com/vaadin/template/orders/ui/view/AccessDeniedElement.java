6b97c82e42df3a13944f815af7059e5147531a0d
Artur Signell <artur@vaadin.com>
2017-05-06 13:38:40 +0300

    Add tests for new orders
    Fixes #98
    
    Removes time part of due date as specified in UX design

---
+++ src/test/java/com/vaadin/template/orders/ui/view/AccessDeniedElement.java
@@ -0,0 +1,3 @@
+package com.vaadin.template.orders.ui.view;
+public class AccessDeniedElement extends AccessDeniedDesignElement {
+}
---
+++ src/test/java/com/vaadin/template/orders/ui/view/admin/product/ProductAdminViewElement.java
@@ -0,0 +1,3 @@
+package com.vaadin.template.orders.ui.view.admin.product;
+public class ProductAdminViewElement extends ProductAdminViewDesignElement {
+}
