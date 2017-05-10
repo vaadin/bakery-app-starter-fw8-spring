6d39e4fe405c7f33175196182b533afbbcdc6ae9
Artur Signell <artur@vaadin.com>
2017-05-10 22:38:24 +0300

    Use service instead of repository for Products and Users

--- src/main/java/com/vaadin/template/orders/ui/components/ProductComboBoxDataProvider.java
+++ src/main/java/com/vaadin/template/orders/ui/components/ProductComboBoxDataProvider.java
@@ -6,36 +6,28 @@ import org.springframework.data.domain.Pageable;
 
 import com.vaadin.data.provider.Query;
 import com.vaadin.spring.annotation.SpringComponent;
-import com.vaadin.template.orders.backend.ProductRepository;
 import com.vaadin.template.orders.backend.data.entity.Product;
+import com.vaadin.template.orders.backend.service.ProductService;
 import com.vaadin.template.orders.ui.view.admin.PageableDataProvider;
 
 @SpringComponent
 public class ProductComboBoxDataProvider extends PageableDataProvider<Product, String> {
 
-	private final ProductRepository productRepository;
+	private final ProductService service;
 
 	@Autowired
-	public ProductComboBoxDataProvider(ProductRepository productRepository) {
-		this.productRepository = productRepository;
-	}
-
-	private String getNameFilter(Query<Product, String> query) {
-		if (query.getFilter().isPresent()) {
-			return "%" + query.getFilter().get() + "%";
-		} else {
-			return "%";
-		}
+	public ProductComboBoxDataProvider(ProductService service) {
+		this.service = service;
 	}
 
 	@Override
 	protected Page<Product> fetchFromBackEnd(Query<Product, String> query, Pageable pageable) {
-		return productRepository.findByNameLikeIgnoreCaseOrderByName(getNameFilter(query), pageable);
+		return service.findAnyMatching(query.getFilter(), pageable);
 	}
 
 	@Override
 	protected int sizeInBackEnd(Query<Product, String> query) {
-		return productRepository.countByNameLikeIgnoreCase(getNameFilter(query));
+		return (int) service.countAnyMatching(query.getFilter());
 	}
 
 }
