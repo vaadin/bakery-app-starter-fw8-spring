2709a878131c3630fdbe56fe1385af5664282f72
Artur Signell <artur@vaadin.com>
2017-05-10 22:51:23 +0300

    Use service instead of repository for Orders

--- src/main/java/com/vaadin/template/orders/ui/components/OrdersDataProvider.java
+++ src/main/java/com/vaadin/template/orders/ui/components/OrdersDataProvider.java
@@ -3,44 +3,29 @@ package com.vaadin.template.orders.ui.components;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.vaadin.data.provider.AbstractBackEndDataProvider;
 import com.vaadin.data.provider.Query;
 import com.vaadin.spring.annotation.SpringComponent;
-import com.vaadin.template.orders.backend.OrderRepository;
 import com.vaadin.template.orders.backend.data.OrderState;
 import com.vaadin.template.orders.backend.data.entity.Order;
-import com.vaadin.template.orders.framework.FrameworkDataHelper;
+import com.vaadin.template.orders.backend.service.OrderService;
 import com.vaadin.template.orders.ui.PrototypeScope;
+import com.vaadin.template.orders.ui.view.admin.PageableDataProvider;
 
 @SpringComponent
 @PrototypeScope
-public class OrdersDataProvider extends AbstractBackEndDataProvider<Order, Object> {
+public class OrdersDataProvider extends PageableDataProvider<Order, Object> {
 
 	@Autowired
-	private OrderRepository orderRepository;
+	private OrderService service;
 
 	@Override
-	protected Stream<Order> fetchFromBackEnd(Query<Order, Object> query) {
-		int firstRequested = query.getOffset();
-		int nrRequested = query.getLimit();
-		Pageable pageable = FrameworkDataHelper.getPageable(query);
-
-		List<Order> items = orderRepository
-				.findByDueDateAfterAndStateInOrderByDueDateAscDueTimeDescIdDesc(getFilterDate(), getStates(), pageable)
-				.getContent();
-		int firstReturned = pageable.getPageNumber();
-		int firstReal = firstRequested - firstReturned;
-		int afterLastReal = firstReal + nrRequested;
-		if (afterLastReal > items.size()) {
-			afterLastReal = items.size();
-		}
-		return items.subList(firstReal, afterLastReal).stream();
-
+	protected Page<Order> fetchFromBackEnd(Query<Order, Object> query, Pageable pageable) {
+		return service.findAfterDueDateWithState(getFilterDate(), getStates(), pageable);
 	}
 
 	private List<OrderState> getStates() {
@@ -53,7 +38,7 @@ public class OrdersDataProvider extends AbstractBackEndDataProvider<Order, Objec
 
 	@Override
 	protected int sizeInBackEnd(Query<Order, Object> query) {
-		return (int) orderRepository.countByDueDateAfterAndStateIn(getFilterDate(), getStates());
+		return (int) service.countAfterDueDateWithState(getFilterDate(), getStates());
 	}
 
 }
