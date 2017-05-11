d85f3b83d40a67cd599e617d54503ffd11727bdf
Artur <artur@vaadin.com>
2017-05-11 18:56:05 +0300

    Use service instead of repository for PickupLocation (#137)

--- src/main/java/com/vaadin/template/orders/ui/components/PickupLocationComboBoxDataProvider.java
+++ src/main/java/com/vaadin/template/orders/ui/components/PickupLocationComboBoxDataProvider.java
@@ -6,32 +6,24 @@ import org.springframework.data.domain.Pageable;
 
 import com.vaadin.data.provider.Query;
 import com.vaadin.spring.annotation.SpringComponent;
-import com.vaadin.template.orders.backend.PickupLocationRepository;
 import com.vaadin.template.orders.backend.data.entity.PickupLocation;
+import com.vaadin.template.orders.backend.service.PickupLocationService;
 import com.vaadin.template.orders.ui.view.admin.PageableDataProvider;
 
 @SpringComponent
 public class PickupLocationComboBoxDataProvider extends PageableDataProvider<PickupLocation, String> {
 
 	@Autowired
-	private PickupLocationRepository repository;
-
-	private String getNameFilter(Query<PickupLocation, String> query) {
-		if (query.getFilter().isPresent()) {
-			return "%" + query.getFilter().get() + "%";
-		} else {
-			return "%";
-		}
-	}
+	private PickupLocationService service;
 
 	@Override
 	protected Page<PickupLocation> fetchFromBackEnd(Query<PickupLocation, String> query, Pageable pageable) {
-		return repository.findByNameLikeIgnoreCaseOrderByName(getNameFilter(query), pageable);
+		return service.findAnyMatching(query.getFilter(), pageable);
 	}
 
 	@Override
 	protected int sizeInBackEnd(Query<PickupLocation, String> query) {
-		return repository.countByNameLikeIgnoreCase(getNameFilter(query));
+		return (int) service.countAnyMatching(query.getFilter());
 	}
 
 }
