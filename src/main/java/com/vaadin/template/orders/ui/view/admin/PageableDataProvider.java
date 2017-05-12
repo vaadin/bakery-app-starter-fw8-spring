18f05faadec1b27a4311dab1b2a6a741dec7959a
Artur <artur@vaadin.com>
2017-05-12 23:24:39 +0300

    Add a paging data provider which always hits the database once (#153)
    Fixes #101

--- src/main/java/com/vaadin/template/orders/ui/view/admin/PageableDataProvider.java
+++ src/main/java/com/vaadin/template/orders/ui/view/admin/PageableDataProvider.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.util.Pair;
 
 import com.vaadin.data.provider.AbstractBackEndDataProvider;
 import com.vaadin.data.provider.Query;
@@ -22,7 +23,26 @@ public abstract class PageableDataProvider<T, F> extends AbstractBackEndDataProv
 	protected abstract Page<T> fetchFromBackEnd(Query<T, F> query, Pageable pageable);
 
 	private static <T, F> Pageable getPageable(Query<T, F> q) {
-		return new PageRequest(0, q.getOffset() + q.getLimit());
+		Pair<Integer, Integer> pageSizeAndNumber = limitAndOffsetToPageSizeAndNumber(q.getOffset(), q.getLimit());
+		return new PageRequest(pageSizeAndNumber.getSecond(), pageSizeAndNumber.getFirst());
+	}
+
+	public static Pair<Integer, Integer> limitAndOffsetToPageSizeAndNumber(int offset, int limit) {
+		int minPageSize = limit;
+		int lastIndex = offset + limit - 1;
+		int maxPageSize = lastIndex + 1;
+
+		for (double pageSize = minPageSize; pageSize <= maxPageSize; pageSize++) {
+			int startPage = (int) (offset / pageSize);
+			int endPage = (int) (lastIndex / pageSize);
+			if (startPage == endPage) {
+				// It fits on one page, let's go with that
+				return Pair.of((int) pageSize, startPage);
+			}
+		}
+
+		// Should not really get here
+		return Pair.of(maxPageSize, 0);
 	}
 
 	private static <T> Stream<T> fromPageable(Page<T> result, Pageable pageable, Query<T, ?> query) {
@@ -30,7 +50,7 @@ public abstract class PageableDataProvider<T, F> extends AbstractBackEndDataProv
 
 		int firstRequested = query.getOffset();
 		int nrRequested = query.getLimit();
-		int firstReturned = pageable.getPageNumber();
+		int firstReturned = pageable.getOffset();
 		int firstReal = firstRequested - firstReturned;
 		int afterLastReal = firstReal + nrRequested;
 		if (afterLastReal > items.size()) {
