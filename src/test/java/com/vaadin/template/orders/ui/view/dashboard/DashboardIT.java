ffee153325fc69ad77b00f03abc2366eb691850d
Artur <artur@vaadin.com>
2017-05-12 21:23:32 +0300

    Add Dashboard tests (#151)
    Fixes #96

--- src/test/java/com/vaadin/template/orders/ui/view/dashboard/DashboardIT.java
+++ src/test/java/com/vaadin/template/orders/ui/view/dashboard/DashboardIT.java
@@ -1,38 +1,83 @@
 package com.vaadin.template.orders.ui.view.dashboard;
 
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.template.orders.AbstractOrdersIT;
+import com.vaadin.template.orders.ui.view.dashboard.DashboardViewElement.BoardBoxElement;
+import com.vaadin.template.orders.ui.view.dashboard.DashboardViewElement.ChartElement;
 import com.vaadin.template.orders.ui.view.object.LoginViewElement;
-import com.vaadin.template.orders.ui.view.orders.ElementUtil;
-import com.vaadin.testbench.TestBenchElement;
-import com.vaadin.testbench.elements.CssLayoutElement;
-import com.vaadin.testbench.elementsbase.ServerClass;
+import com.vaadin.testbench.By;
 
 public class DashboardIT extends AbstractOrdersIT {
 
-	@ServerClass("com.vaadin.template.orders.ui.view.dashboard.BoardBox")
-	public static class BoardBoxElement extends CssLayoutElement {
+	// Test does not depend heavily on data, instead assert that all values make
+	// sense and not everything is zeroes
 
-		public String getHeader() {
-			return ElementUtil.getText((TestBenchElement) findElement(By.className("board-box-small-text")));
-		}
+	@Test
+	public void containsCharts() {
+		DashboardViewElement dashboardView = LoginViewElement.loginAsAdmin();
 
-		public String getContent() {
-			return ElementUtil.getText((TestBenchElement) findElement(By.className("board-box-big-text")));
-		}
+		ChartElement deliveriesThisMonth = dashboardView.getDeliveriesThisMonth();
+		String thisMonth = YearMonth.now().getMonth().getDisplayName(TextStyle.FULL, Locale.US);
+		Assert.assertEquals("Deliveries in " + thisMonth, deliveriesThisMonth.getTitle());
+		Assert.assertEquals(1, deliveriesThisMonth.getSeries().size());
+		Assert.assertTrue(deliveriesThisMonth.hasData());
+
+		ChartElement deliveriesThisYear = dashboardView.getDeliveriesThisYear();
+		Assert.assertEquals("Deliveries in " + Year.now().getValue(), deliveriesThisYear.getTitle());
+		Assert.assertEquals(1, deliveriesThisYear.getSeries().size());
+		Assert.assertTrue(deliveriesThisYear.hasData());
+
+		ChartElement salesLastYears = dashboardView.getYearlySales();
+		Assert.assertEquals(3, salesLastYears.getSeries().size());
+		Assert.assertTrue(deliveriesThisYear.hasData());
+
+		ChartElement productSplit = dashboardView.getMonthlyProductSplit();
+		List<WebElement> series = productSplit.getSeries();
+		Assert.assertEquals(1, series.size());
+		Assert.assertTrue("With the generated data, more than 3 products should have been sold last month",
+				series.get(0).findElements(By.tagName("path")).size() > 3);
 	}
 
 	@Test
-	public void dashboardContainsData() {
+	public void boxesContainData() {
 		DashboardViewElement dashboardView = LoginViewElement.loginAsAdmin();
-		List<WebElement> boxes = dashboardView.getBoard().findElements(By.className("board-box-label"));
+		List<BoardBoxElement> boxes = dashboardView.getBoxes();
 		Assert.assertEquals(4, boxes.size());
 
+		BoardBoxElement todayBox = dashboardView.getTodayBox();
+		String[] todayValues = todayBox.getContent().split("/", 2);
+		int deliveredToday = Integer.parseInt(todayValues[0]);
+		int totalToday = Integer.parseInt(todayValues[1]);
+
+		Assert.assertTrue("With the generated data, there should be at least one delivery", totalToday > 0);
+		Assert.assertTrue("Delivered cannot be larger than total", deliveredToday <= totalToday);
+
+		BoardBoxElement naBox = dashboardView.getNotAvailableBox();
+		int naToday = Integer.parseInt(naBox.getContent());
+		Assert.assertTrue("N/A data cannot be negative", naToday >= 0);
+
+		BoardBoxElement newBox = dashboardView.getNewBox();
+		int nrNew = Integer.parseInt(newBox.getContent());
+		Assert.assertTrue("With the generated data, there should be more than one new order", nrNew > 1);
+
+		BoardBoxElement tomorrowBox = dashboardView.getTomorrowBox();
+		int nrTomorrow = Integer.parseInt(tomorrowBox.getContent());
+		Assert.assertTrue("With the generated data, there should be at least one order tomorrow", nrTomorrow > 0);
+	}
+
+	@Test
+	public void gridContainsData() {
+		DashboardViewElement dashboardView = LoginViewElement.loginAsAdmin();
+		Assert.assertTrue("With the generated data, there should be at least ten rows in the grid",
+				dashboardView.getGrid().getRowCount() > 10);
 	}
 }
--- src/test/java/com/vaadin/template/orders/ui/view/dashboard/DashboardViewElement.java
+++ src/test/java/com/vaadin/template/orders/ui/view/dashboard/DashboardViewElement.java
@@ -1,3 +1,85 @@
 package com.vaadin.template.orders.ui.view.dashboard;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.template.orders.ui.view.orders.ElementUtil;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elements.AbstractComponentElement;
+import com.vaadin.testbench.elements.CssLayoutElement;
+import com.vaadin.testbench.elements.GridElement;
+import com.vaadin.testbench.elementsbase.ServerClass;
+
 public class DashboardViewElement extends DashboardViewDesignElement {
+
+	@ServerClass("com.vaadin.template.orders.ui.view.dashboard.BoardBox")
+	public static class BoardBoxElement extends CssLayoutElement {
+
+		public String getHeader() {
+			return ElementUtil.getText((TestBenchElement) findElement(By.className("board-box-small-text")));
+		}
+
+		public String getContent() {
+			return ElementUtil.getText((TestBenchElement) findElement(By.className("board-box-big-text")));
+		}
+	}
+
+	@ServerClass("com.vaadin.addon.charts.Chart")
+	public static class ChartElement extends AbstractComponentElement {
+
+		public String getTitle() {
+			return findElement(By.className("highcharts-title")).getText();
+		}
+
+		public boolean hasData() {
+			return findElements(By.className("highcharts-no-data")).isEmpty();
+		}
+
+		public List<WebElement> getSeries() {
+			return findElements(By.className("highcharts-series"));
+		}
+	}
+
+	public List<BoardBoxElement> getBoxes() {
+		return $(BoardBoxElement.class).all();
+	}
+
+	public BoardBoxElement getTodayBox() {
+		return $(BoardBoxElement.class).get(0);
+	}
+
+	public BoardBoxElement getNotAvailableBox() {
+		return $(BoardBoxElement.class).get(1);
+	}
+
+	public BoardBoxElement getNewBox() {
+		return $(BoardBoxElement.class).get(2);
+	}
+
+	public BoardBoxElement getTomorrowBox() {
+		return $(BoardBoxElement.class).get(3);
+	}
+
+	public ChartElement getDeliveriesThisMonth() {
+		return $(ChartElement.class).id("deliveriesThisMonth");
+	}
+
+	public ChartElement getDeliveriesThisYear() {
+		return $(ChartElement.class).id("deliveriesThisYear");
+	}
+
+	public ChartElement getYearlySales() {
+		return $(ChartElement.class).id("yearlySales");
+	}
+
+	public ChartElement getMonthlyProductSplit() {
+		return $(ChartElement.class).id("monthlyProductSplit");
+	}
+
+	public GridElement getGrid() {
+		return $(GridElement.class).id("dueGrid");
+	}
+
 }
