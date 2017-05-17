980be3f902e389874937759f12d8cb369d8e7af2
Artur <artur@vaadin.com>
2017-05-17 14:05:05 +0300

    Allow updating a password in the user admin but don't show the encrypted one (#166)
    Fixes #108

--- src/test/java/com/vaadin/template/orders/ui/view/admin/user/UserAdminViewElement.java
+++ src/test/java/com/vaadin/template/orders/ui/view/admin/user/UserAdminViewElement.java
@@ -1,3 +1,11 @@
 package com.vaadin.template.orders.ui.view.admin.user;
+
+import com.vaadin.template.orders.ui.CurrentDriver;
+import com.vaadin.testbench.ElementQuery;
+
 public class UserAdminViewElement extends UserAdminViewDesignElement {
+
+	public static UserAdminViewElement get() {
+		return new ElementQuery<>(UserAdminViewElement.class).context(CurrentDriver.get()).first();
+	}
 }
