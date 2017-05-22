df87367d41b8fb5bb876346e4c5217312111a573
Artur Signell <artur@vaadin.com>
2017-05-22 14:10:41 +0300

    Make logout work again

--- src/test/java/com/vaadin/template/orders/ui/view/LoginIT.java
+++ src/test/java/com/vaadin/template/orders/ui/view/LoginIT.java
@@ -2,9 +2,11 @@ package com.vaadin.template.orders.ui.view;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import com.vaadin.template.orders.AbstractOrdersIT;
 import com.vaadin.template.orders.ui.view.object.LoginViewElement;
+import com.vaadin.template.orders.ui.view.object.MenuElement;
 import com.vaadin.template.orders.ui.view.orders.OrderEditViewElement;
 
 public class LoginIT extends AbstractOrdersIT {
@@ -15,4 +17,12 @@ public class LoginIT extends AbstractOrdersIT {
 		OrderEditViewElement orderEditView = OrderEditViewElement.get();
 		Assert.assertEquals("#1", orderEditView.getOrderId().getText());
 	}
+
+	@Test
+	public void logoutWorks() {
+		LoginViewElement.loginAsBarista();
+		MenuElement.get().logout();
+		Assert.assertEquals("Email", findElement(By.id("login-label")).getText());
+	}
+
 }
