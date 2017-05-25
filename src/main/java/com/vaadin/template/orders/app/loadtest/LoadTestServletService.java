ed52da5d8ec0152690b6407ca0cded67eed6927a
Artur Signell <artur@vaadin.com>
2017-05-25 09:49:47 +0300

    Remove unnecessary dependency on Vaadin4Spring

--- src/main/java/com/vaadin/template/orders/app/loadtest/LoadTestServletService.java
+++ src/main/java/com/vaadin/template/orders/app/loadtest/LoadTestServletService.java
@@ -5,23 +5,21 @@ import com.vaadin.server.ServiceException;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.server.VaadinSession;
-import org.vaadin.spring.servlet.Vaadin4SpringServletService;
+import com.vaadin.spring.server.SpringVaadinServletService;
 
 /**
  * Extended VaadinServletService to create LoadTestServletSession to enable
  * stable load testing with component ids used for communication
  */
-public class LoadTestServletService extends Vaadin4SpringServletService {
+public class LoadTestServletService extends SpringVaadinServletService {
 
-    public LoadTestServletService(VaadinServlet servlet,
-            DeploymentConfiguration deploymentConfiguration, String serviceUrl)
-            throws ServiceException {
-        super(servlet, deploymentConfiguration, serviceUrl);
-    }
+	public LoadTestServletService(VaadinServlet servlet, DeploymentConfiguration deploymentConfiguration,
+			String serviceUrl) throws ServiceException {
+		super(servlet, deploymentConfiguration, serviceUrl);
+	}
 
-    @Override
-    protected VaadinSession createVaadinSession(VaadinRequest request)
-            throws ServiceException {
-        return new LoadTestServletSession(this);
-    }
+	@Override
+	protected VaadinSession createVaadinSession(VaadinRequest request) throws ServiceException {
+		return new LoadTestServletSession(this);
+	}
 }
