ffd9a2589043eb925580148d435a5530f4f15b57
Artur <artur@vaadin.com>
2017-05-30 11:19:22 +0300

    Remove dead code (#267)

--- src/main/java/com/vaadin/template/orders/backend/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.template.orders.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vaadin.template.orders.backend.data.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
