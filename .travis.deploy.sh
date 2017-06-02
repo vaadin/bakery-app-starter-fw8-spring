32e43c9ba0573e7420aa3d2bc26ac3f584ab6238
Artur Signell <artur@vaadin.com>
2017-06-02 11:09:40 +0300

    Fix war deployment for renamed artifact

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -P 5188 target/orders-1.0-SNAPSHOT.war dev@app.fi:orders.war
+scp -o StrictHostKeyChecking=no -P 5188 target/bakery-app-starter-fw8-spring-*.war dev@app.fi:orders.war
 ssh -o StrictHostKeyChecking=no -p 5188 dev@app.fi mv orders.war tomcat/webapps/ROOT.war
 
