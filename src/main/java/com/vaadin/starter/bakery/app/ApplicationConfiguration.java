dfc365f039664517e32309c0b4a3ddde4b919b19
Artur Signell <artur@vaadin.com>
2017-06-14 22:03:09 +0300

    Merge servlets into one and extract id generator logic
    Still uses a separate LoadTestServletService because the Framework API is awkward
    Extracts IconBootstrapListener to a separate class to keep servlet as simple as possible

--- src/main/java/com/vaadin/starter/bakery/app/ApplicationConfiguration.java
+++ src/main/java/com/vaadin/starter/bakery/app/ApplicationConfiguration.java
@@ -1,41 +1,19 @@
 package com.vaadin.starter.bakery.app;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.vaadin.data.converter.StringToIntegerConverter;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.starter.bakery.app.loadtest.LoadTestVaadinSpringServlet;
-
 @Configuration
 public class ApplicationConfiguration {
 
 	/**
-	 * Is load test mode enabled (false by default)
+	 * The password encoder to use when encrypting passwords.
 	 */
-	@Value("${loadtestmode.enabled}")
-	private Boolean loadTestModeEnabled;
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	public StringToIntegerConverter stringToIntegerConverter() {
-		return new StringToIntegerConverter("Unable to parse integer");
-	}
-
-	@Bean
-	VaadinServlet vaadinServlet() {
-		if (loadTestModeEnabled) {
-			// Use Customized Vaadin servlet for the load test mode
-			return new LoadTestVaadinSpringServlet();
-		} else {
-			return new ApplicationServlet();
-		}
-	}
 }
