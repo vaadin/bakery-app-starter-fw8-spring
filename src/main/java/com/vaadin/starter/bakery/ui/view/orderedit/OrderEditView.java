11cf679e1fc2030607120bbc868bcc1f3199b56c
Artur <artur@vaadin.com>
2017-06-16 15:17:06 +0300

    Move ConfirmPopup to .components (#382)

--- src/main/java/com/vaadin/starter/bakery/ui/view/orderedit/OrderEditView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/orderedit/OrderEditView.java
@@ -21,9 +21,9 @@ import com.vaadin.starter.bakery.app.BeanLocator;
 import com.vaadin.starter.bakery.backend.data.OrderState;
 import com.vaadin.starter.bakery.backend.data.entity.Order;
 import com.vaadin.starter.bakery.backend.data.entity.OrderItem;
+import com.vaadin.starter.bakery.ui.components.ConfirmPopup;
 import com.vaadin.starter.bakery.ui.util.DollarPriceConverter;
 import com.vaadin.starter.bakery.ui.view.NavigableView;
-import com.vaadin.starter.bakery.ui.view.confirmpopup.ConfirmPopup;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Label;
 
