a6d88a32fe178c5abb673da2f9e21b8eae12f7d1
Leif Åstrand <leif@vaadin.com>
2017-06-27 13:15:33 +0300

    Simplify init listener registration
    Let the app's init listener be a spring managed bean to simplify
    registration (no file needed in META-INF/services) and access of value
    from application.properties. Depends on
    https://github.com/vaadin/spring/pull/223.

--- src/main/java/com/vaadin/starter/bakery/app/ApplicationInitListener.java
+++ src/main/java/com/vaadin/starter/bakery/app/ApplicationInitListener.java
@@ -3,14 +3,13 @@ package com.vaadin.starter.bakery.app;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import com.vaadin.server.ConnectorIdGenerator;
 import com.vaadin.server.ServiceInitEvent;
 import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServiceInitListener;
-import com.vaadin.server.VaadinServletService;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.starter.bakery.app.loadtest.ComponentIdBasedConnectorIdGenerator;
 
@@ -23,7 +22,11 @@ import com.vaadin.starter.bakery.app.loadtest.ComponentIdBasedConnectorIdGenerat
  * Uses a bootstrap listener to modify the bootstrap HTML page and include icons
  * for home screen for mobile devices.
  */
+@Component
 public class ApplicationInitListener implements VaadinServiceInitListener {
+
+    @Value("${loadtestmode.enabled}")
+    private boolean loadTestModeEnabled;
 
     @Override
     public void serviceInit(ServiceInitEvent serviceInitEvent) {
@@ -32,7 +35,7 @@ public class ApplicationInitListener implements VaadinServiceInitListener {
         service.addSessionInitListener(event -> event.getSession()
                 .addBootstrapListener(new IconBootstrapListener()));
 
-        if (isLoadTestEnabled(service)) {
+        if (loadTestModeEnabled) {
             Logger.getLogger(ApplicationInitListener.class.getName()).log(
                     Level.WARNING,
                     "Running in load test mode, do not use this in production");
@@ -41,24 +44,6 @@ public class ApplicationInitListener implements VaadinServiceInitListener {
                     event -> getGenerator(event.getSession())
                             .generateConnectorId(event));
         }
-    }
-
-    private static boolean isLoadTestEnabled(VaadinService service) {
-        /*
-         * Must manually find the application property since service init
-         * listeners are not Spring managed beans.
-         *
-         * See https://github.com/vaadin/spring/issues/213 and
-         * https://github.com/vaadin/spring/pull/218.
-         */
-        WebApplicationContext applicationContext = WebApplicationContextUtils
-                .getWebApplicationContext(((VaadinServletService) service)
-                        .getServlet().getServletContext());
-
-        Boolean loadTestMode = applicationContext.getEnvironment()
-                .getProperty("loadtestmode.enabled", Boolean.class);
-
-        return Boolean.TRUE.equals(loadTestMode);
     }
 
     private static ConnectorIdGenerator getGenerator(VaadinSession session) {
