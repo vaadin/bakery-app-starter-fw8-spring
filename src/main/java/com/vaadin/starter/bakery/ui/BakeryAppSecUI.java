58900a843b23d1ae91aed033879e3c16c92f8437
tamasmak <maktamas@gmail.com>
2023-07-11 12:47:59 +0200

    Add @SpringUI annotation for AppSecUI

---
+++ src/main/java/com/vaadin/starter/bakery/ui/BakeryAppSecUI.java
@@ -0,0 +1,8 @@
+package com.vaadin.starter.bakery.ui;
+
+import com.vaadin.appsec.v8.ui.AppSecUI;
+import com.vaadin.spring.annotation.SpringUI;
+
+@SpringUI(path = "/appsec")
+public class BakeryAppSecUI extends AppSecUI {
+}
