63051f5fd6771a39d31ce63120a33702960a8ebf
tamasmak <maktamas@gmail.com>
2023-08-04 10:04:52 +0200

    Remove not needed BakeryAppSecUI

--- src/main/java/com/vaadin/starter/bakery/ui/BakeryAppSecUI.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.starter.bakery.ui;
-
-import com.vaadin.appsec.v8.ui.AppSecUI;
-import com.vaadin.spring.annotation.SpringUI;
-
-@SpringUI(path = "/appsec")
-public class BakeryAppSecUI extends AppSecUI {
-}
